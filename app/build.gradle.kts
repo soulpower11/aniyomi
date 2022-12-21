8191c0c887d66134e78f3282fb1c7a35f7fa1ff3
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2022-12-21 14:59:07 +0800

    release test update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = AndroidConfig.minSdk
         targetSdk = AndroidConfig.targetSdk
         versionCode = 93
-        versionName = "0.14.2.0"
+        versionName = "0.14.2.0b"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
